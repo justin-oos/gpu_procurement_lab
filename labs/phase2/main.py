7a84ebd37fb901a079e2eb33a327cc40bec61f8e
Justin Randall <justinran@google.com>
2025-12-03 23:16:53 +0000

    lic: Added Apache-2.0 license to all files.

--- labs/phase2/main.py
+++ labs/phase2/main.py
@@ -1,13 +1,22 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import asyncio
 import logging
 import os
 import traceback
 
-# --- MONKEY PATCH ---
-# The following code is a temporary workaround for an issue in the google-adk library.
-# It prevents an unhandled exception when a ParallelAgent is used.
-# The issue is that a GeneratorExit is not handled in the _merge_agent_run function.
-# This patch wraps the while loop in a try...except GeneratorExit block.
 from typing import AsyncGenerator
 from google.adk.events.event import Event
 from google.adk.agents import parallel_agent
